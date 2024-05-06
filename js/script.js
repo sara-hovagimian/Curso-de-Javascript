498fd61b12962259c50551ad29e14e337c394bd9
Sara Hovagimian <sarahovamanuk@gmail.com>
2024-05-06 11:58:22 -0300

    agregamos archivo readme.md

---
+++ js/script.js
@@ -0,0 +1,31 @@
+//mostrar en la consola un saludo, nombre y mensaje de error
+const SALUDO="Hola";
+console.log("SALUDO");
+console.log("Sara");
+console.error("asi se imprime un Error en la consola");
+
+//ingresar y mostrar nombre
+let nombre=prompt("ingrese su nombre");
+console.log(nombre);
+
+let edad=prompt("Ingrese su edad");
+console.log(edad);
+
+//ingresar y mostrar mensaje de bienvinda con alert
+alert ("bienvenido a la clase 1 del curso de havascript");
+
+//preguntar por la comida preferida y mostrarla con alert
+let comida=prompt("cual es tu comida favorita?");
+alert (comida);
+
+//pedir nombre y edad al usuario
+const BLANCO=" ";
+let nombre1=prompt("ingrese su nombre");
+let edad1=prompt("ingrese su edad");
+alert ("bienvenido"+BLANCO+nombre1+BLANCO+"tienes"+BLANCO+edad1+BLANCO+"años");
+
+//año de nacimiento del usuario
+let anio=2024-edad1
+alert ("tu año de nacimiento es"+BLANCO+anio )
+
+//ver si es menor o mayor de edad
---
+++ readme.md
@@ -0,0 +1,7 @@
+## Ejercico 1 del curso Javascript
+Son 3 ejercicios del profesor.
+- ingrese el nombre
+- ingres la edad
+- muestrelo en pantalla
+[Sitio de atencion al cliente](http://sac.bas.com.ar "Sitio de atencion al cliente")
+![](https://img.freepik.com/foto-gratis/paisaje-forestal_71767-127.jpg)
